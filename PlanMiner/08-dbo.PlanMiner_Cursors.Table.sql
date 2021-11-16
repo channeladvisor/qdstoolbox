5ad7ead821f7c8be4886d700fa8647da6536f7d3
pablo.lozano <lozano_pablo@hotmail.com>
2021-11-16 05:01:58 -0500

    Fix for issue #23
    Added missing [StatementID] column to [dbo].[PlanMiner_Cursors]

--- PlanMiner/08-dbo.PlanMiner_Cursors.Table.sql
+++ PlanMiner/08-dbo.PlanMiner_Cursors.Table.sql
@@ -6,6 +6,9 @@
 -- Columns:
 --		[PlanMinerID]			BIGINT			NOT NULL
 --			Unique identifier of the mined plan
+--
+--		[StatementID]			INT				NOT NULL
+--			Identifier of the statement within the plan the cursor is used in
 --			
 --		[CursorName]			NVARCHAR(128)	NULL
 --			Name of the cursor
@@ -25,12 +28,17 @@
 -- Date: 2021.05.08
 -- Auth: Pablo Lozano (@sqlozano)
 --
+-- Date: 2021.11.16
+-- Auth: Pablo Lozano (@sqlozano)
+-- Note: Added [StatementID] column missing in the table definition
+--			https://github.com/channeladvisor/qdstoolbox/issues/23
 ----------------------------------------------------------------------------------
 
 DROP TABLE IF EXISTS [dbo].[PlanMiner_Cursors]
 CREATE TABLE [dbo].[PlanMiner_Cursors]
 (
 	 [PlanMinerID]			BIGINT			NOT NULL
+	,[StatementID]			INT				NOT NULL
 	,[CursorName]			NVARCHAR(128)	NULL
 	,[CursorActualType]		NVARCHAR(128)	NULL
 	,[CursorRequestedType]	NVARCHAR(128)	NULL
