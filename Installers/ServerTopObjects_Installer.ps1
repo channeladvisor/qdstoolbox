1325e34f06724015628a856c127e76e92ca11976
Pablo Lozano Fernández <lozano_pablo@hotmail.com>
2022-10-18 12:22:04 +0200

    New ServerTopObjects SP
    Added new objects to support ServerTopObjects, and corrected typos and bugs found in ServerTopQueries

---
+++ Installers/ServerTopObjects_Installer.ps1
@@ -0,0 +1,26 @@
+Param(
+    [Parameter(mandatory=$true)][string]$TargetInstance,
+    [Parameter(mandatory=$true)][string]$TargetDatabase,
+    [string]$TargetSchema = "dbo",
+    [string]$Login,
+    [string]$Password
+)
+Import-Module SqlServer
+
+# Deploy all SQL script found in \ServerTopObjects
+$SQLScripts = (Get-ChildItem -Path '..\ServerTopObjects' -Filter "*.sql") | Sort
+foreach($Script in $SQLScripts){
+    # Replace default schema name [dbo] with [$TargetSchema]
+    $ScriptContents = Get-Content -Path $Script.FullName -Raw
+    $ScriptContents = ($ScriptContents.Replace("[dbo]","[$($TargetSchema)]"))
+
+    # Deploy updated script
+    if($Login){
+        # Login / Password authentication
+        Invoke-SqlCmd -ServerInstance $TargetInstance -Database $TargetDatabase -Username $Login -Password $Password -Query $ScriptContents
+    }
+    else {
+        # Active Directory authentication
+        Invoke-SqlCmd -ServerInstance $TargetInstance -Database $TargetDatabase -Query $ScriptContents
+        }  
+}
